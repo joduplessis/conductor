7d4160dbac8fb65a90dc5c9b4a4e5670dd03a78e
Jo du Plessis <jo@joduplessis.com>
2019-09-20 10:24:34 +0200

    Removed redundant logic for routes

--- src/Route.tsx
+++ src/Route.tsx
@@ -2,24 +2,24 @@ import * as React from "react";
 import { ThemeContext } from "./ThemeContext";
 
 function matchPathToUrl(path: string, url: string) {
-  const sanitizedUrl: string = url[0] == '/' ? url.substring(1) : url;
-  const sanitizedPath: string = path[0] == '/' ? path.substring(1) : path;
-  const sanitizedUrlParts: string[] = sanitizedUrl.split('/');
-  const sanitizedPathParts: string[] = sanitizedPath.split('/');
-  const pathBoundaries: any[] = sanitizedPath.split('/').map(part => part[0] == ':' ? -1 : part);
+  const sanitizedUrl: string = url[0] == "/" ? url.substring(1) : url;
+  const sanitizedPath: string = path[0] == "/" ? path.substring(1) : path;
+  const sanitizedUrlParts: string[] = sanitizedUrl.split("/");
+  const sanitizedPathParts: string[] = sanitizedPath.split("/");
+  const pathBoundaries: any[] = sanitizedPath.split("/").map(part => part[0] == ":" ? -1 : part);
   let values: any = {};
 
   const passes = pathBoundaries.reduce((pass: boolean, part: string, index: number) => {
     if (sanitizedUrlParts.length != sanitizedPathParts.length) return false;
 
-    // If the part doesn't equal -1
+    // If the part doesn"t equal -1
     // then the 2 should be the same
     // otherwise return false
     if (part != -1) {
       if (sanitizedPathParts[index] != sanitizedUrlParts[index]) return false;
     } else {
       // If the part does equal -1
-      // then it's a vairable
+      // then it"s a vairable
       // We need to check that it exists
       if (sanitizedUrlParts[index] == undefined || sanitizedUrlParts[index] == null) return false;
 
@@ -41,7 +41,7 @@ interface IRouteProps {
 }
 
 export const Route: React.FunctionComponent<IRouteProps> = (props: IRouteProps) => {
-  const [currentUrl, setCurrentUrl] = React.useState('')
+  const [currentUrl, setCurrentUrl] = React.useState("")
   const [render, setRender] = React.useState(false)
 
   return (
@@ -50,18 +50,15 @@ export const Route: React.FunctionComponent<IRouteProps> = (props: IRouteProps) 
         const path: string = props.path;
         const url = window.location.pathname;
 
-        // If the URL doesn't change - don't recalcualte
-        if (url == currentUrl && render) setRender(true)
-        if (url == currentUrl && !render) setRender(false)
-
-        // If the URL changes
+        // If the URL doesn"t change - don"t recalcualte
+        // If the URL changes:
         // Decide if we show the component
         if (url != currentUrl) {
           setRender(matchPathToUrl(path, url))
           setCurrentUrl(url)
         }
 
-        // Only render it if it's the right route
+        // Only render it if it"s the right route
         if (!render) return null
 
         // Pass down any additional props
